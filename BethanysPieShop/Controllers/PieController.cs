13f667e1381aed9f4a02e3a011a5ee4500fcbe45
claudia-caripa <claudia.caripa@gmail.com>
2022-07-15 14:32:30 +0100

    Add project files.

---
+++ BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,39 @@
+﻿using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        //Create an action method, List, that is going to go to the repository and ask for all the pies to be returned 
+
+        //First create a readonly instance of IPieRepository and ICategoryRepository
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        //Initialize them through the use of the constructor
+        //Here we are using constructor injection because we registered the IPieRepository and ICategoryRepository in the Program, so an instance of MockPieRepository and MockCategoryRepository will be injected
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        
+        //Create the first action method
+        //The return type is IActionResult, that is the overarching interface type that all results will implement 
+        public IActionResult List()
+        {
+            //ViewBag is dynamic, you can add any property onto it
+            // ViewBag is shared between the controller and the view
+            //The view will be able to access the properties inside of the ViewBag
+            ViewBag.CurrentCategory = "Cheese cakes";
+
+            //For now this will return a View passing in he _pieRepository.AllPies
+            return View(_pieRepository.AllPies);
+        }
+        
+
+
+    }
+}
---
+++ BethanysPieShop/Program.cs
@@ -0,0 +1,52 @@
+//Program.cs is the only file that uses top-level statements
+//This file will really launch the application as a console application 
+
+/* WebApplication.CreateBuilder makes sure that the app at startup looks at
+* the appsettings.json and loads the settings from that file
+* It will also make sure that Kestrelis inclided and set up IS integration 
+* Another default applied by CreateBuilder is that the wwwroot folder will the 
+* folder to contain static content 
+*/
+using BethanysPieShop.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Let's add in a service here
+
+//Register ourown services: ICategoryRepository and IPieRepository
+builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+builder.Services.AddScoped<IPieRepository, MockPieRepository>();
+
+//builder has access to Servecies collections
+//AddControlersWithViews will make sure that our application knows about ASP.NET Core MVC
+builder.Services.AddControllersWithViews();
+
+//This app instance can be used to bring in multiple middleware components
+var app = builder.Build();
+
+/*Midddelware components build up what is known as the middleware request pipeline
+* By defualt MapGet is a middelware component tha listens
+* for an incoming request to the root of the app "/" and then return Hello World
+* At this point: send a GET, gte back Hello World
+* app.MapGet("/", () => "Hello World!");
+* */
+
+//Add a middleware component
+app.UseStaticFiles(); //It looks for incoming requests for static files (JPEG, CSS file)
+
+//Anothe middleware to see errors inside the executing application, 
+// It must be used development enviroment, not production or staging 
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+
+// Middleware to have the ability to be able to navigate to our pages (views),
+// to in fact make sure that ASP.NET Core will be able to handle incoming requests correctly
+// This is an endpoint middleware so it must be placed at the end
+app.MapDefaultControllerRoute();
+
+//This will start our application 
+app.Run();
